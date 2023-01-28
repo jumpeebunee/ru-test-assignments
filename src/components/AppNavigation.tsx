57df6854640feb910200a044fac203bbcdadb256
Ben <jumpeebunee@gmail.com>
2023-01-28 12:39:55 +0300

    feat: add routes

---
+++ src/components/AppNavigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import MainPage from '../pages/MainPage';
+import ValutesPage from '../pages/ValutesPage';
+
+const AppNavigation = () => {
+  return (
+    <Routes>
+      <Route path={'/'} element={<MainPage/>}></Route>
+      <Route path={'/valutes'} element={<ValutesPage/>}></Route>
+    </Routes>
+  )
+}
+
+export default AppNavigation
---
+++ src/pages/ValutesPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ValutesPage = () => {
+  return (
+    <div>ValutesPage</div>
+  )
+}
+
+export default ValutesPage

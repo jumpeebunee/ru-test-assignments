7822466053a9f699a152093189c5bb3066755ec6
Ben <jumpeebunee@gmail.com>
2023-01-28 12:46:45 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './styles/App.scss';
+import './styles/normolize.css';
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
--- src/pages/ValutesPage.tsx
+++ src/pages/ValutesPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const ValutesPage = () => {
   return (
     <div>ValutesPage</div>
